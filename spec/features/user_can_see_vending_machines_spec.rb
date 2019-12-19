195ff1d21af5477592e3d2aac86b405b5e894c9a
Lain McGratH <lain.mcgrath@gusto.com>
2019-12-19 10:38:27 -0700

    Add another snack to test

--- spec/features/user_can_see_vending_machines_spec.rb
+++ spec/features/user_can_see_vending_machines_spec.rb
@@ -6,9 +6,20 @@ RSpec.describe "When a user visits the vending machine index", type: :feature do
     sam.machines.create(location: "Don's Mixed Drinks")
     sam.machines.create(location: "Turing Basement")
 
+
     visit owner_machines_path(sam)
 
     expect(page).to have_content("Don's Mixed Drinks")
     expect(page).to have_content("Turing Basement")
+  end
+
+  xit "can see all snacks" do
+    sam = Owner.create(name: "Sam's Snacks")
+    machine = sam.machines.create(location: "Don's Mixed Drinks")
+
+    visit machine_path(sam)
+    new_snack = Snack.create(name: "Oreos")
+
+    machine << new_snack
   end
 end
